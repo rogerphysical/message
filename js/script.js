f28526c1f64369168a6debabe222092ab506a201
rogerphysical <55873122+rogerphysical@users.noreply.github.com>
2020-06-17 20:52:53 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function save_data() {
     var time = d.getFullYear() + "-" + zero(d.getMonth() + 1) + "-" + zero(d.getDate()) + " " + zero(d.getHours()) + ":" + zero(d.getMinutes()) + ":" + zero(d.getSeconds());
     var datas = {"time": time, "who": named, "con": data.value};
     var datas_json = JSON.stringify(datas);
-    localStorage.setItem(d.getTime(), datas_json);
+    localStorage.setItem("message_"+d.getTime(), datas_json);
     data.value = "";
     write_data();
   }
@@ -32,20 +32,30 @@ function write_data() {
   // var com = document.getElementById("comment");
   // com.innerHTML = "";
   for(var i = 0; i < localStorage.length; i++) {
-    datas = localStorage.getItem(localStorage.key(i));
-    var datas_c = JSON.parse(datas);
+    var item = localStorage.key(i);
+    if (item.substr(0, 8) === "message_") {
+      datas = localStorage.getItem(item);
+      var datas_c = JSON.parse(datas);
+      dataHtml += "<div class=\"comment\"><div class=\"comment1\">" + datas_c.who + "</div><div class=\"comment2\">" + datas_c.time + "</div><div class=\"comment3\">" + datas_c.con + "</div></div>";
+    }
+    
     // var newdiv = document.createElement('div');
     // newdiv.setAttribute('class','comment');
     // newdiv.innerHTML = "<div>" + datas_c.who + "</div>" + "<div>" + datas_c.time + "</div>" + "<div>" + datas_c.con + "</div>";
     // com.appendChild(newdiv);
-    dataHtml += "<div class=\"comment\"><div class=\"comment1\">" + datas_c.who + "</div><div class=\"comment2\">" + datas_c.time + "</div><div class=\"comment3\">" + datas_c.con + "</div></div>";
+    
   }
   document.getElementById("comment").innerHTML = dataHtml;
 }
 function delete_data() {
   var judge = confirm("確定清空留言嗎?");
   if (judge == true) {
-    localStorage.clear();
+    for(var i = 0; i < localStorage.length; i++) {
+      var item = localStorage.key(i);
+      if (item.substr(0, 8) === "message_") {
+        localStorage.removeItem(item);
+      }
+    }
     write_data();
   }
 }
